da9f76bafc137dabb56ace068358c6b8a7b445f5
Jose Ramon Cano Yribarren <jcano@ravenpack.com>
2020-12-17 02:04:34 +0100

    :art: Autoformatting

--- slack/views/slack.py
+++ slack/views/slack.py
@@ -5,34 +5,35 @@ from flask import Blueprint, jsonify, request
 from partyparrot import convert, convert_with_alphabet_emojis
 from ..utils import parse_emojify_args
 
-slack = Blueprint('slack', __name__)
+slack = Blueprint("slack", __name__)
 
-logger = getLogger('slack')
+logger = getLogger("slack")
 logger.setLevel(DEBUG)
-file_handler = FileHandler('app.log')
+file_handler = FileHandler("app.log")
 file_handler.setLevel(DEBUG)
-formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
-DEFAULT_FG_EMOJI = ':partyparrot:'
-DEFAULT_BG_EMOJI = ':nbsp:'
+DEFAULT_FG_EMOJI = ":partyparrot:"
+DEFAULT_BG_EMOJI = ":nbsp:"
 
-@slack.route('/', methods=['GET'])
+
+@slack.route("/", methods=["GET"])
 def slack_test():
-    return 'Slack api is running'
+    return "Slack api is running"
 
 
-@slack.route('/emojify', methods=['GET'])
+@slack.route("/emojify", methods=["GET"])
 def emojify_test():
-    return 'Emojify uses POST'
+    return "Emojify uses POST"
 
 
-@slack.route('/emojify', methods=['POST'])
+@slack.route("/emojify", methods=["POST"])
 def emojify():
-    text = request.form['text']
-    user_name = request.form['user_name']
-    logger.debug(f'user {user_name} emojified {text}')
+    text = request.form["text"]
+    user_name = request.form["user_name"]
+    logger.debug(f"user {user_name} emojified {text}")
     args = parse_emojify_args(text)
     message = args[0]
     text = convert_message(message, args[1:])
